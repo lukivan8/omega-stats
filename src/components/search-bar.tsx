d57b65dfdd9875a3b2494c2a8bad8fd33e58be56
lukivan <lukivan888@gmail.com>
2023-06-11 21:39:26 +0600

    fixed dropdown behaviour

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -14,8 +14,6 @@ export default function SearchBar() {
   const path = usePathname();
   const [inputValue, setInputValue] = useState("");
   const [suggestions, setSuggestions] = useState<PlayerData[]>([]);
-  const [focused, setFocused] = useState(false);
-  const [hovered, setHovered] = useState(false);
 
   const handleInputChange = (e: any) => {
     const input = e.target.value;
@@ -34,18 +32,13 @@ export default function SearchBar() {
   };
 
   return (
-    <div>
+    <div className="dropdown">
+      {/* Search bar */}
       <form>
         <div className="flex w-full justify-center border rounded items-center py-1 px-2 ">
           <input
             value={inputValue}
             onChange={handleInputChange}
-            onFocus={() => {
-              setFocused(true);
-            }}
-            onBlur={() => {
-              setFocused(false);
-            }}
             className="text-base bg-base-100 border-white px-1 rounded w-full focus:outline-none md:p-2"
             placeholder="Username"
           />
@@ -60,26 +53,24 @@ export default function SearchBar() {
           <div className="relative "></div>
         </div>
       </form>
-      {suggestions.length > 0 && (focused || !hovered) && (
-        <ul
-          onMouseOver={() => {
-            setHovered(true);
-          }}
-          onMouseDown={() => {
-            setHovered(false);
-          }}
-          className="p-2 absolute shadow-lg menu dropdown-content bg-base-100 rounded-box w-52"
-        >
-          {suggestions.map((suggestion, index) => (
-            <li
-              key={index}
-              onClick={() => handleSuggestionClick(suggestion)}
-              className="px-2 py-2 cursor-pointer text-ellipsis hover:underline"
-            >
-              {suggestion.username}
-            </li>
-          ))}
-        </ul>
+      {/* Suggestions */}
+      {suggestions.length > 0 && (
+        <div className="relative">
+          <ul
+            tabIndex={0}
+            className="p-2 w-full absolute shadow-lg menu z-10 dropdown-content bg-base-100 rounded-box"
+          >
+            {suggestions.map((suggestion, index) => (
+              <li
+                key={index}
+                onClick={() => handleSuggestionClick(suggestion)}
+                className="px-2 py-2 mt-[2px] bg-base-100 cursor-pointer hover:bg-neutral-focus"
+              >
+                {suggestion.username}
+              </li>
+            ))}
+          </ul>
+        </div>
       )}
     </div>
   );
